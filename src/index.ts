defd2b26861293b4eab7e06625be7ca192b4e934
Rye <git@itsrye.dev>
2026-06-29 15:22:42 +0200

    tsconfig decorator config

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { Result, unwrap, unwrapOr, unwrapOrThrow, safeUnwrap, isSuccess, isError
 export { ResultWrapper, resultify } from './result_wrapper'; 
 export { $unwrapOrNull, $unwrapOr } from './macros';
 export { ResultMacros } from './plugin';
-export { ThrowableResultPacker } from './decorators';
+export { ThrowableResultPacker, packThrowable } from './decorators';
