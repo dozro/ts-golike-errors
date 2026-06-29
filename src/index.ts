88c8a48ec2fe9794f7e42f4da3a974f1965b397d
Rye <git@itsrye.dev>
2026-06-29 15:33:30 +0200

    removed broken code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { Result, unwrap, unwrapOr, unwrapOrThrow, safeUnwrap, isSuccess, isError } from './result';
 export { ResultWrapper, resultify } from './result_wrapper'; 
 export { $unwrapOrNull, $unwrapOr } from './macros';
-export { ResultMacros } from './plugin';
 export { ThrowableResultPacker, packThrowable } from './decorators';
