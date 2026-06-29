ac1a0c611058200b330ec70cd82049f63a90023b
Rye <git@itsrye.dev>
2026-06-29 10:50:18 +0200

    macros plugin

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Result, unwrap, unwrapOr, unwrapOrThrow, safeUnwrap, isSuccess, isError } from './result';
 export { ResultWrapper, resultify } from './result_wrapper'; 
 export { $unwrapOrNull, $unwrapOr } from './macros';
+export { ResultMacros } from './plugin';
